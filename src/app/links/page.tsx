43417d38b7afe69ebd2c8974e31e8078acf58d64
Austin Bearden <austinb99@4jlt.com>
2024-05-17 08:49:23 -0400

    Updated some styles

---
+++ src/app/links/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Links from "./Links";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-between">
+      <Links />
+    </main>
+  );
+}
--- src/app/profile/home/Profile.tsx
+++ src/app/profile/home/Profile.tsx
@@ -20,23 +20,23 @@ interface ProfileProps {
 export default function Profile({ }: ProfileProps) {
     const profileData = profile as ProfileDataInterface;
 
-    const buttonStyles = "w-full h-[100px] bg-gray-600 bg-opacity-70 justify-self-center m-2 rounded-lg shadow-lg";
+    const buttonStyles = "w-full h-[100px] bg-gray-600 bg-opacity-70 justify-self-center m-2 p-2 rounded-lg shadow-lg whitespace-break-spaces";
    
     return (
-        <div className="h-full w-full grid grid-rows-3">
+        <div className="h-full sm:w-full md:w-full lg:w-2/3 w-full grid grid-rows-3">
             <div className="flex flex-col items-end">
-                <img src={profileBackgroundImage.src} className="absolute w-full bg-fixed bg-top h-1/4 min-h-[150px] object-cover blur-sm" alt="profile image" />
+                <img src={profileBackgroundImage.src} className="absolute xs:w-full sm:w-full md:w-full lg:w-2/3 w-full bg-fixed bg-top xs:h-1/5 sm:h-1/5 md:h-1/4 lg:h-1/4 h-1/5 min-h-[150px] object-cover blur-sm" alt="profile image" />
                 <div className="z-10 w-full flex flex-row justify-end items-center">
                     <span className="flex flex-col justify-evenly items-end h-1/2 mr-4">
-                        <h2 className="text-5xl text-gray-600 text-end">{profileData.name}</h2>
-                        <h4 className="text-3xl text-gray-500 text-end">{profileData.title}</h4>
-                        <h5 className="text-2xl text-blue_font text-end">{profileData.tagLine}</h5>
+                        <h2 className="sm:text-2xl md:text-3xl lg:text-5xl text-2xl text-gray-600 text-end">{profileData.name}</h2>
+                        <h4 className="sm:text-2xl lg:text-3xl text-xl text-red-400 text-end">{profileData.title}</h4>
+                        <h5 className="sm:text-lg md:text-xl lg:text-2xl text-lg text-gray-600 text-end">{profileData.tagLine}</h5>
                     </span>
                     <img src={profileData.profileImageUrl} className="shadow-lg rounded-full max-w-[400px] w-1/5 h-auto m-4" alt="profile image" />
                 </div>
             </div>
             <div id="body" className="w-full h-full flex flex-col justify-start items-center">
-                <div id="description" className="w-11/12 h-full bg-blue_font bg-opacity-50 p-8 rounded-2xl shadow-lg">{profileData.description}</div>
+                <div id="description" className="w-11/12 h-full bg-blue_font bg-opacity-50 p-8 rounded-2xl shadow-lg overflow-y-auto">{profileData.description}</div>
             </div>
             <div id="footer" className="w-full mt-10 flex flex-col justify-between items-center">
                 <div id="navigation" className="w-11/12 flex flex-row justify-center items-baseline">
