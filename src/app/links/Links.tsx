f33895dcf0ea00edc484487efbc3f79eb5de1513
Austin Bearden <austinb99@4jlt.com>
2024-05-18 00:30:10 -0400

    Added profile links page

--- src/app/links/Links.tsx
+++ src/app/links/Links.tsx
@@ -1,11 +1,16 @@
+"use client"
+
 import { profile } from "@/data/profile";
+import SecondaryLayout from "../SecondaryLayout";
+import { LinkPreview } from "@dhaiwat10/react-link-preview";
 interface ProfileDataInterface {
     name: string,
     profileImageUrl: string,
     title: string,
     description: string,
     githubUrl: string,
-    linkedInUrl: string
+    linkedInUrl: string,
+    sourceCodeUrl: string
 }
 
 export default function Links() {
@@ -20,20 +25,25 @@ export default function Links() {
         window.open(profileData.githubUrl, "_blank");
     }
 
+    const openSourceCode = () => {
+        window.open(profileData.sourceCodeUrl, "_blank");
+    }
+
+    const buttonStyle = "flex flex-col justify-center items-center bg-gray-500 bg-opacity-40 rounded-2xl m-1";
+
     return (
-        <div id="links" className="lg:h-1/2 lg:grid lg:grid-cols-2 lg:p-1">
-                <button id="github" onClick={() => openGitHubProfile()} className="flex flex-col justify-center items-center bg-gray-500 bg-opacity-40 rounded-2xl m-1">
-                    <h4>GitHub</h4>
+        <SecondaryLayout>
+            <div id="links" className="h-1/2 w-3/4 self-center grid grid-cols-2 p-1 grid-flow-row">
+                <button id="github" onClick={() => openGitHubProfile()} className={buttonStyle}>
+                    <h4>Github</h4>
                 </button>
-                <button id="linkedin" onClick={() => openLinkedInProfile()} className="flex flex-col justify-center items-center bg-gray-500 bg-opacity-40 rounded-2xl m-1">
+                <button id="linkedin" onClick={() => openLinkedInProfile()} className={buttonStyle}>
                     <h4>Linkedin</h4>
                 </button>
-                <button id="recommendations" className="flex flex-col justify-center items-center bg-gray-500 bg-opacity-40 rounded-2xl m-1">
-                    <h4>Recommendations</h4>
-                </button>
-                <button id="source-code" className="flex flex-col justify-center items-center bg-gray-500 bg-opacity-40 rounded-2xl m-1">
+                <button id="source-code" onClick={() => openSourceCode()} className={buttonStyle}>
                     <h4>Source Code</h4>
                 </button>
             </div>
+        </SecondaryLayout>
     );
 }
--- src/data/profile.ts
+++ src/data/profile.ts
@@ -5,5 +5,6 @@ export const profile = {
     "description": "As a Software Engineer at 3Aware, I've applied my creativity and passion for software development to build innovative and user-friendly applications using React and Java Spring Boot on both AWS and GCP. I led the front-end development and introduced standards and best practices for React, Javascript, and design.",
     "tagLine": "React, Typescript, Java, AWS",
     "githubUrl": "https://github.com/aibearden",
-    "linkedInUrl": "https://www.linkedin.com/in/austin-bearden-3a7466266/"
+    "linkedInUrl": "https://www.linkedin.com/in/austin-bearden-3a7466266/",
+    "sourceCodeUrl": "https://github.com/aibearden/austinbearden"
 }
