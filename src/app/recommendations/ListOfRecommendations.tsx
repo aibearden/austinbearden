868c86f83b96533916a91b94fd59248176dc7ff3
Austin Bearden <austinb99@4jlt.com>
2024-05-19 16:29:20 -0400

    Updated grid for ListOfRecommendations to move header

--- src/app/recommendations/ListOfRecommendations.tsx
+++ src/app/recommendations/ListOfRecommendations.tsx
@@ -8,11 +8,11 @@ export default function ListOfRecommendations() {
 
     return (
         <SecondaryLayout>
-            <div className="h-full w-2/3 max-w-[680px] self-center grid grid-rows-4">
+            <div className="h-full w-2/3 max-w-[680px] self-center grid grid-rows-8">
                 <div className="row-span-1 flex flex-row justify-center content-end">
                     <h2 className="text-2xl self-center">Recommendations</h2>
                 </div>
-                <div className="w-full h-full overflow-y-auto row-span-3 flex flex-col justify-start content-center">
+                <div className="w-full h-full overflow-y-auto row-span-7 flex flex-col justify-start">
                     {(recommendations as RecommendationInterface[]).map((recommendation) => (
                         <Recommendation key={recommendation.author} recommendation={recommendation} />
                     ))}
