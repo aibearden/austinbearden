109a1ffaf523ca237f32fcb6d805f9cc74151b38
Austin Bearden <austinb99@4jlt.com>
2024-05-21 09:15:40 -0400

    Updated profile url

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,6 +1,6 @@
 export const profile = {
     "name": "Austin Bearden",
-    "profileImageUrl": "https://media.licdn.com/dms/image/D5603AQEyf1boly3GbQ/profile-displayphoto-shrink_800_800/0/1715441823575?e=1721260800&v=beta&t=CLkKLuXN3HnupLqZlc2UY4APVwcPoZGbYGBCXeKxI-c",
+    "profileImageUrl": "https://media.licdn.com/dms/image/D5603AQF6RmTmpD5PMA/profile-displayphoto-shrink_800_800/0/1716209124042?e=1721865600&v=beta&t=qBDFV_YZe460EIyVWu1IE7BQyDRaXww85u-qFxECwsM",
     "title": "Software Engineer",
     "description": "As a Software Engineer at 3Aware, I've applied my creativity and passion for software development to build innovative and user-friendly applications using React and Java Spring Boot on both AWS and GCP. I led the front-end development and introduced standards and best practices for React, Javascript, and design.",
     "tagLine": "React, Typescript, Java, AWS",
