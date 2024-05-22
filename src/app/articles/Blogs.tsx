0a97ef9e5cca4134f52f3b24cd13f73bd4ac15af
Austin Bearden <austinb99@4jlt.com>
2024-05-21 21:36:38 -0400

    Changed order

--- src/app/articles/Blogs.tsx
+++ src/app/articles/Blogs.tsx
@@ -12,7 +12,7 @@ export default function Blogs() {
                 <h2 className="text-2xl w-full h-[1/5] p-4 text-center">Articles</h2>
                 <div className="flex flex-col content-center h-[4/5] overflow-y-auto">
                     {articleData.map((article: ArticleInterface, index: number) => {
-                        return <Article article={article} key={index} />
+                        return <Article key={index} article={article} />
                     })}
                 </div>
             </div>
